f8917305b1c6837865bd95336c7cefaf80e80c22
Corinne Padilla <corinne padilla>
2022-05-18 19:34:29 -0700

    tom garrett and tom helped

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ console.log ("Italian")
 //This is a single line comment//
 
 /*This is a multi line comment*/
+
+console.log ("Supercalifragilisticexpialidocious".length);
+console.log (14);
+console.log (20n);
+console.log (typeof 14);
+console.log (typeof 20n);
+
